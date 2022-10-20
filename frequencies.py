c3d55abc535df488e4e92574bc76c24cd4900e20
George Korodimos <61911511+LuxionDev@users.noreply.github.com>
2022-10-20 17:59:40 +0000

    completed exercise

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
-    return frequencies
+    freq = {}
+    if len(items) > 0:
+        if len(freq) == 0:
+            freq[str(items[0])] = 0
+        for item in items:
+            print(str(item))
+            if str(item) in freq.keys():
+                freq[str(item)] += 1
+            else:
+                freq[str(item)] = 1
+
+    print(freq)    
+    return freq
